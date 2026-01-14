e8591335ac129df1ba2326f5805bc262dcf6692b
Q7os <kyahhui12@gmail.com>
2026-01-14 14:27:19 +0300

    Replace 'VR7' text with 'Q7o' in GUI elements

--- Q7o.lua
+++ Q7o.lua
@@ -1,11 +1,13 @@
+-- تشغيل السكربت الأصلي
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hm5011/hussain/refs/heads/main/Mercy%20Script"))()
 
+-- تغيير جميع النصوص "VR7" إلى "Q7o"
 task.spawn(function()
     local player = game:GetService("Players").LocalPlayer
     local gui = player:WaitForChild("PlayerGui")
 
     while true do
-        for _,v in pairs(gui:GetDescendants()) do
+        for _, v in pairs(gui:GetDescendants()) do
             if (v:IsA("TextLabel") or v:IsA("TextButton")) and v.Text then
                 v.Text = v.Text:gsub("VR7", "Q7o")
             end
